aae3cfd1f06ba214577b5512bc1a4341c9b01e9d
thenm-2k <thenm.2k@gmail.com>
2022-07-08 13:49:41 +0700

    create folder italian-food-thenm

---
+++ italian-food-thenm/js/main.js
@@ -0,0 +1,10 @@
+$('.variable-width').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    arrows: false,
+    centerPadding: '0'
+});
